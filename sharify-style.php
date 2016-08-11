e87ba5c41da148e7dd1eb7c4078e95a0da5653de
Christopher Rolfe <christopher.rolfe198@gmail.com>
2016-08-11 15:12:42 +0100

    Properly referencing wp_load.php
    Adding the function call to use instead of exploding the url as it doesn't always work.

--- sharify-style.php
+++ sharify-style.php
@@ -1,5 +1,5 @@
 <?php
- $absolute_path = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
+ $absolute_path = get_home_path();
  $wp_load = $absolute_path[0] . 'wp-load.php';
  require_once($wp_load);
 
